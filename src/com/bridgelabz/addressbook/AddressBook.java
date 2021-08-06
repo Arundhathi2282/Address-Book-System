6e53cf6fbf60824a249dc23bb0c7cd0dbc441c28
Arundhathi2282 <arundhathikuricheti@gmail.com>
2021-08-06 15:50:58 +0530

    Add : Welcome to the Address Book System

---
+++ src/com/bridgelabz/addressbook/AddressBook.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.addressbook;
+
+public class AddressBook {
+	/**
+	 * Main method from where the execution will start, Displaying Welcome message
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book System....!");
+
+	}
+
+}
