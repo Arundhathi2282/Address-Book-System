c47ecd90f228fa4af4cf43c794aa1aac449a61d6
Arundhathi2282 <arundhathikuricheti@gmail.com>
2021-08-07 12:22:22 +0530

    Add : Edit the existing contact in the Address book using name

--- src/com/bridgelabz/addressbook/AddressBook.java
+++ src/com/bridgelabz/addressbook/AddressBook.java
@@ -17,8 +17,23 @@ public class AddressBook {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address Book System....!");
 		AddressBook addressBook = new AddressBook();
-		addressBook.addContact();
+		addressBook.performOperations();
+	}
 
+	public void performOperations() {
+		System.out.println("Enter 1 or 2");
+		int option = SC.nextInt();
+		switch (option) {
+		case 1:
+			addContact();
+			break;
+		case 2:
+			editContact();
+			break;
+		default:
+			System.out.println("Enter a valid number");
+		}
+		yesOrNo();
 	}
 
 	/**
@@ -44,7 +59,61 @@ public class AddressBook {
 		person.setEmail(SC.next());
 		personList.add(person);
 		System.out.println(personList);
-		yesOrNo();
+	}
+
+	/**
+	 * Editing the existing contact in the Address Book
+	 */
+	public void editContact() {
+		System.out.println("Enter User name to edit the contact");
+		String firstName = SC.next();
+		for (int i = 0; i < personList.size(); i++) {
+			System.out.println(personList);
+			String name = personList.get(i).getFirstName();
+			if (name.equals(firstName)) {
+				System.out.println(
+						"Choose from menu which index you want to edit\n1.First Name\n2.Last Name\n3.Address\n4.City\n5.State\n6.Zip\n7.Phone Number\n8.Email");
+				int choice = SC.nextInt();
+				// Person person = new Person();
+				switch (choice) {
+				case 1:
+					System.out.println("New First Name : ");
+					personList.get(i).setFirstName(SC.next());
+					break;
+				case 2:
+					System.out.println("New Last Name : ");
+					personList.get(i).setLastName(SC.next());
+					break;
+				case 3:
+					System.out.println("New Address : ");
+					personList.get(i).setLastName(SC.next());
+					break;
+				case 4:
+					System.out.println("New City : ");
+					personList.get(i).setLastName(SC.next());
+					break;
+				case 5:
+					System.out.println("New State : ");
+					personList.get(i).setLastName(SC.next());
+					break;
+				case 6:
+					System.out.println("New Zip : ");
+					personList.get(i).setLastName(SC.next());
+					break;
+				case 7:
+					System.out.println("New Phone Number : ");
+					personList.get(i).setLastName(SC.next());
+					break;
+				case 8:
+					System.out.println("New Email : ");
+					personList.get(i).setLastName(SC.next());
+					break;
+				default:
+					System.out.println("Enter a valid number");
+				}
+				System.out.println(personList);
+			}
+		}
 	}
 
 	/**
@@ -55,7 +124,7 @@ public class AddressBook {
 		System.out.println("Choose one Y or N");
 		char choice = SC.next().charAt(0);
 		if (choice == 'Y' || choice == 'y') {
-			addContact();
+			performOperations();
 		}
 
 	}
